c84c245db72f9624b867609a8cdf04b00f727ce0
ZJF1997 <40564676+Zou-JianFeng@users.noreply.github.com>
2019-02-12 22:34:21 +0800

    C语言math.h使用

---
+++ math.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pow.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2019/02/11 11时35分33秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    printf("%f\n", pow(2, 3));
+    printf("%f\n", sqrt(16));
+    printf("%f\n", ceil(16.1));
+    printf("%f\n", log(9));
+    printf("%f\n", log10(100));
+    return 0;
+}
